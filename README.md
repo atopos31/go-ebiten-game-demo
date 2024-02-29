dbba7ba582a6b28e8b8775c0fc76f4bd9c510b76
hackerxiao <2945294768@qq.com>
2024-02-29 15:36:47 +0800

    add: README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# 用Ebitengine写一个飞机大战
+![效果](./index/assets/show.png)
+## 本地运行
+```bash
+go run ./index/main.go
+```
+## 浏览器wasm运行
+```bash
+go run ./server/server.go
+```
+## [教程地址](https://www.hackerxiao.online/2023/11/11/ebitengine_air/)
--- server/server.go
+++ server/server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Game running in http://127.0.0.1:8080/wasm_exec.html")
 	if err := http.ListenAndServe(":8080", http.FileServer(http.Dir("./index"))); err != nil {
 		log.Fatal(err)
 	}
